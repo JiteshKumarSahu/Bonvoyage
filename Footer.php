80de4a490cb05ca88ccfd08176583b6fd8add306
JiteshKumarSahu <50472750+JiteshKumarSahu@users.noreply.github.com>
2019-05-11 20:03:39 +0530

    HomePage
    Airline service Home page here you can search the flights, and special offer provide for travelling

---
+++ Footer.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>footer</title>
+        <link href="Style/Footer.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <div class="last">
+            <h4 style="text-align: center;">BONVOYAGE AIRLINE.....</h4> 
+            <div class="last1"><p>Bon Voyage, is your online travel agent that offers you a one stop travel shop for cheap flight bookings,online flight reservations, holidays & package tours,& more.We help you find cheapest air ticket bookings on all
+                    Indian Airlines flight deals. We scan fares on both full service airlines such as Jet airways, Air India and low cost carriers such as
+                    <br> Spicejet, Go Air and Indigo to find you the lowest air fare on flight tickets.The Bon Voyage travel bazaar also features luxury vacations 
+                    <br>& budget tour packages including tour packages for Indian holiday packages. One can also avail of discount indian hotels,& the most suited holiday packages with Bon Voyage.</p></div>
+            <div class="last2">
+                <ul class="fmenu">
+                    <li class="insidemenu"><a href="Home.php">HOME</a></li>
+                    <li class="insidemenu"><a href="Flights.php">FLIGHT</a></li>
+                    <li class="insidemenu"><a href="Holidays.php">HOLIDAYS</a></li>
+                    <li class="insidemenu"><a href="Xplore.php">X-PLORE</a></li>
+                    <li class="insidemenu"><a href="Luxory.php">LUXURY TRAVEL</a></li>
+                </ul>
+            </div>
+            <div class="last3"><i class="fab fa-whatsapp" style="font-size: 20px; margin-left: 30px; color:greenyellow"></i><h4 style="margin-left: 55px; margin-top: -20px;">Contact us:   881906024</div>
+            <div class="last4">
+                <div class="icon"> <i class="fab fa-twitter"></i></div>
+                <div class="icon1"><i class="fab fa-facebook-f"></i></div>
+                <div class="icon2"><i class="fab fa-instagram"></i></div>
+                <div class="icon3"><i class="fab fa-google-plus-g"></i></div>
+                <div class="icon4"><i class="fab fa-linkedin-in"></i></div>
+            </div>
+            <div class="last5">
+                <p style="margin-left:50px; "><img src="images/flag-for-india.png" height="50%" width="10%">
+                    Bonvoyage Airways(india)Ltd.All rights reserved.</p>
+            </div>
+        </div>
+        
+    </body>
+    
+</html>
